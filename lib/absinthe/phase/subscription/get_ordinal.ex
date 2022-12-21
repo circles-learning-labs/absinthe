35d1dd9e257ae0faa5606b8c794312633fc57849
Nikolai Davydov <nikolai.davydov96@googlemail.com>
2022-12-21 12:14:24 +0100

    moved comment to pullrequest instead

--- lib/absinthe/phase/subscription/get_ordinal.ex
+++ lib/absinthe/phase/subscription/get_ordinal.ex
@@ -10,12 +10,6 @@ defmodule Absinthe.Phase.Subscription.GetOrdinal do
   @spec run(any, Keyword.t()) :: {:ok, Blueprint.t()}
   def run(blueprint, _options \\ []) do
     with %{type: :subscription, selections: [field]} <- Blueprint.current_operation(blueprint),
-         # I commented out the following line because it causes problems for me
-         # and since I do not use the ordinal feature I can not properly test it for you so I will refrain from changing anything here
-         # The reason why this causes a problem is that I (and I assume many others) have side effects in their subscription config function.
-         # And this causes it to be called multiple times. This breaks my entire app and will probably have to be
-         # done differently unless you want the library to have breaking changes for many users
-         # {:ok, config} = SubscribeSelf.get_config(field, blueprint.execution.context, blueprint)
          {:ok, config} = {:ok, %{}},
          ordinal_fun when is_function(ordinal_fun, 1) <- config[:ordinal] do
       result = ordinal_fun.(blueprint.execution.root_value)
